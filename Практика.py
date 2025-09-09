1cf9ea8e6a87a2658cf197434d5874df2cdc3c45
Arikama-sudo <mariagrayfer@mail.ru>
2025-09-09 18:09:33 +0500

    2

--- Практика.py
+++ Практика.py
@@ -17,50 +17,50 @@
 # else:
 #     print("Нет такого дня недели")
 #2
-month = int(input("Введите номер месяца:"))
-if month == 1:
-    print("Январь")
-elif month == 2:
-    print("Февраль")
-elif month ==3:
-    print("Март")
-elif month ==4:
-    print("Апрель")
-elif month ==5:
-    print("Май")
-elif month ==6:
-    print("Июнь")
-elif month ==7:
-    print("Июль")
-elif month == 8:
-    print("Август")
-elif month == 9:
-    print("Сентябрь")
-elif month == 10:
-    print("Октябрь")
-elif month == 11:
-    print("Ноябрь")
-elif month == 12:
-    print("Декабрь")
-else:
-    print("Нет такого номера месяца")
-#3
-number = int(input("Введите число:"))
-if number > 0:
-    print("Number is positive")
-elif number < 0:
-    print("Nimber is negative")
-elif number == 0:
-    print("Number is equal to zero")
-else:
-    print("...")
-#4
-number1 = int(input("Введите число:"))
-number2 = int(input("Введите число:"))
-if number1 == number2:
-    print("Равно")
-elif number1 > number2:
-    print("1 > 2")
-elif number1 < number2:
-    print("1 < 2")
-else:
+# month = int(input("Введите номер месяца:"))
+# if month == 1:
+#     print("Январь")
+# elif month == 2:
+#     print("Февраль")
+# elif month ==3:
+#     print("Март")
+# elif month ==4:
+#     print("Апрель")
+# elif month ==5:
+#     print("Май")
+# elif month ==6:
+#     print("Июнь")
+# elif month ==7:
+#     print("Июль")
+# elif month == 8:
+#     print("Август")
+# elif month == 9:
+#     print("Сентябрь")
+# elif month == 10:
+#     print("Октябрь")
+# elif month == 11:
+#     print("Ноябрь")
+# elif month == 12:
+#     print("Декабрь")
+# else:
+#     print("Нет такого номера месяца")
+# #3
+# number = int(input("Введите число:"))
+# if number > 0:
+#     print("Number is positive")
+# elif number < 0:
+#     print("Nimber is negative")
+# elif number == 0:
+#     print("Number is equal to zero")
+# else:
+#     print("...")
+# #4
+# number1 = int(input("Введите число:"))
+# number2 = int(input("Введите число:"))
+# if number1 == number2:
+#     print("Равно")
+# elif number1 < number2:
+#     print(number1, number2)
+# else:
+#     print(number2, number1)
+#1
